4e9f304d408d8ff8b549759cd8b12f3631f82e61
Christian Braun <christian.braun@kurzdigital.com>
2018-08-20 19:01:21 +0200

    Initial commit

---
+++ Whale/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whale
+//
+//  Created by Christian Braun on 20.08.18.
+//  Copyright © 2018 KURZ Digital Solutions GmbH & Co. KG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
